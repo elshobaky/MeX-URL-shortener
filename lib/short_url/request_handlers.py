8078747843c547d16e9c98a501a90e7df6a8aa57
Mahmoud Elshobaky <mahmoud.elshobaky@gmail.com>
2016-02-07 15:56:22 +0200

    Add Handlers for Multi URL shortening

--- lib/short_url/request_handlers.py
+++ lib/short_url/request_handlers.py
@@ -9,29 +9,43 @@ _ = t
 from data_models import *
 
 class ShortUrl(MainHandler):
-    def get(self):
-        url = self.request.get('url')
-        title = self.request.get('title')
-        description = self.request.get('description')
-        direct = self.request.get('direct')
-        if direct == "true":
-            direct = True
+    def post(self):
+        self.url = self.request.get('url')
+        self.multi_url = self.request.get('multi_url')
+        self.title = self.request.get('title')
+        self.description = self.request.get('description')
+        self.direct = self.request.get('direct')
+        if self.direct == "true":
+            self.direct = True
         else:
-            direct = False
-        if not valid_url(url):
+            self.direct = False
+        if self.local_user :
+            self.user_id = self.local_user.key.id()
+        else:
+            self.user_id = None
+        if self.multi_url != "false":
+            self.short_multi()
+            return
+        if not valid_url(self.url):
             res = {'error': {'error':'URL not valid'},
-                   'url': url,
-                   'title': title,
-                   'description': description}
+                   'url': self.url,
+                   'title': self.title,
+                   'description': self.description}
             self.write_json(res)
             return
-        if self.local_user :
-            user_id = self.local_user.key.id()
-        else:
-            user_id = None
-        u = Url.short_url(url, title, description,direct=direct, user_id=user_id)
+        u = Url.short_url(self.url, self.title, self.description,direct=self.direct, user_id=self.user_id)
         self.write_json(u.make_json())
         return
+    def short_multi(self):
+        to_short = self.multi_url.split('\n')
+        shortened = []
+        for u in to_short:
+            if not valid_url(u):
+                shortened.append(['error','not valid url ("{}")'.format(u)])
+                continue
+            u_shortened = Url.short_url(u, self.title, self.description,direct=self.direct, user_id=self.user_id)
+            shortened.append(['success',u_shortened.short])
+        self.write_json({'multi_short':True, 'urls':shortened})
 
 class GetUrl(MainHandler):
     def get(self, short):
--- style/js/melshox.js
+++ style/js/melshox.js
@@ -22,6 +22,10 @@ function enableButtons () {
 		$("#result").addClass('hidden');
 		$("#short-form").removeClass('hidden');
 	});
+	// Set action for multi short anchor
+	$("#multi-short").click(multiShort);
+	// Set action for single short anchor
+	$("#single-short").click(singleShort);
 
 	//$("#copy-btn").click(copyResultToClipboard());
 
@@ -32,16 +36,23 @@ function enableButtons () {
 
 function shortURL () {
 	// Get the requested fields
-	var url = document.getElementById("url_field").value;
+	if (document.getElementById("multi_url_field")) {
+		var multi_url = document.getElementById("multi_url_field").value;
+		var url = false;
+	}
+	else {
+		var multi_url = false;
+		var url = document.getElementById("url_field").value;
+	}
 	var direct = document.getElementById("direct-field").checked;
 	var title = document.getElementById("title_field").value;
 	var description = document.getElementById("description_field").value;
-	var fields = {'url': url,'direct': direct,'title':title,'description':description} ;
+	var fields = {'url': url,'multi_url':multi_url,'direct': direct,'title':title,'description':description} ;
 
 	$('#url-input').removeClass('error-field');
 
 	$.ajax({
-		type     : 'GET',
+		type     : 'POST',
 		url      : '/url/short',
 		data     : fields,
 		dataType : 'json',
@@ -49,6 +60,24 @@ function shortURL () {
 		success  : function(data){
 			if (data.error) {
 				$('#url-input').addClass('error-field');
+			}
+			else if (data.multi_short) {
+				var res = '';
+				var rootPath = "http://" + window.location.host + "/";
+				for (u in data.urls) {
+					if (data.urls[u][0] === 'error') {
+						res += data.urls[u][1];
+					}
+					else {
+						res += rootPath;
+						res += data.urls[u][1];
+					}
+					res += '\n'
+				}
+				var res_html = '<textarea id="short-result">'+res+'</textarea>';
+				$('#short-result').replaceWith(res_html);
+				$('#short-form').addClass('hidden');
+				$('#result').removeClass('hidden');
 			}
 			else {
 				var rootPath = "http://" + window.location.host + "/";
@@ -60,33 +89,46 @@ function shortURL () {
 	});
 }
 
+function multiShort() {
+	var multi_input = '<textarea id="multi_url_field" name="multi_url"';
+	multi_input += ' placeholder="enter each URL in separate line">';
+	multi_input += '</textarea>'
+	$("#url_field").replaceWith(multi_input);
+}
+
+function singleShort() {
+	var single_input = '<input id="url_field" name="url" type="url"';
+	single_input += ' placeholder="URL ex: http://example.com">';
+	$("#multi_url_field").replaceWith(single_input);
+}
+
 function copyResultToClipboard() {
 	'use strict';
 	// click events
 	document.body.addEventListener('click', copy, true);
 	// event handler
 	function copy(e) {
-	// find target element
-	var
-		t = e.target,
-		c = t.dataset.copytarget,
-		inp = (c ? document.querySelector(c) : null);
-	// is element selectable?
-	if (inp && inp.select) {
-		// select text
-		inp.select();
-		try {
-			// copy text
-			document.execCommand('copy');
-			inp.blur();
-			// copied animation
-			$("#copy-btn-in").addClass('copied')
-			$("#copy-btn-in").html('Copied ▲');
-			setTimeout(function() { $("#copy-btn-in").html("Copy");$("#copy-btn-in").removeClass('copied'); }, 1500);
+		// find target element
+		var
+			t = e.target,
+			c = t.dataset.copytarget,
+			inp = (c ? document.querySelector(c) : null);
+		// is element selectable?
+		if (inp && inp.select) {
+			// select text
+			inp.select();
+			try {
+				// copy text
+				document.execCommand('copy');
+				inp.blur();
+				// copied animation
+				$("#copy-btn-in").addClass('copied')
+				$("#copy-btn-in").html('Copied ▲');
+				setTimeout(function() { $("#copy-btn-in").html("Copy");$("#copy-btn-in").removeClass('copied'); }, 1500);
+			}
+			catch (err) {
+				alert('please press Ctrl/Cmd+C to copy');
+			}
 		}
-		catch (err) {
-			alert('please press Ctrl/Cmd+C to copy');
 		}
-	}
-	}
 }
